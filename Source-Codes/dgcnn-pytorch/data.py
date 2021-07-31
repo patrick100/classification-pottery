05131868f79da82bd3d706eba7489a267a6949b7
patrick <patrickdz96@gmail.com>
2021-07-31 17:42:18 -0500

    Update data.py

--- Source-Codes/dgcnn-pytorch/data.py
+++ Source-Codes/dgcnn-pytorch/data.py
@@ -39,8 +39,8 @@ def load_data(partition):
     #for h5_name in glob.glob(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5'%partition)):
     #for h5_name in glob.glob(os.path.join(DATA_DIR, 'pottery_no_normalized', '%s*.h5'%partition)):
     #for h5_name in glob.glob(os.path.join(DATA_DIR, 'pottery_normalized', '%s*.h5'%partition)):
-    #for h5_name in glob.glob(os.path.join(DATA_DIR, 'peruvian_normalized', '%s*.h5'%partition)):
-    for h5_name in glob.glob(os.path.join(DATA_DIR, 'peruvian_no_normalized', '%s*.h5'%partition)):
+    for h5_name in glob.glob(os.path.join(DATA_DIR, 'peruvian_normalized', '%s*.h5'%partition)):
+    #for h5_name in glob.glob(os.path.join(DATA_DIR, 'peruvian_no_normalized', '%s*.h5'%partition)):
         f = h5py.File(h5_name)
         data = f['data'][:].astype('float32')
         label = f['label'][:].astype('int64')
