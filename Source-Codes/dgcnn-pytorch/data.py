34962ed6f4eae50dd85b93460bd7187fbd0ee1ae
patrick <patrickdz96@gmail.com>
2021-07-31 17:40:47 -0500

    Update data.py

--- Source-Codes/dgcnn-pytorch/data.py
+++ Source-Codes/dgcnn-pytorch/data.py
@@ -21,13 +21,14 @@ def download():
     DATA_DIR = os.path.join(BASE_DIR, 'data')
     if not os.path.exists(DATA_DIR):
         os.mkdir(DATA_DIR)
+    """
     if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
         www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
         zipfile = os.path.basename(www)
         os.system('wget --no-check-certificate %s; unzip %s' % (www, zipfile))
         os.system('mv %s %s' % (zipfile[:-4], DATA_DIR))
         os.system('rm %s' % (zipfile))
-
+    """
 
 def load_data(partition):
     download()
